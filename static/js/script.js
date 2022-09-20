d06b26abe58bcd3fbef62686805e45f7acdb315b
ming <59531725+newcdn@users.noreply.github.com>
2022-09-20 07:11:43 +0000

    Add files via upload

---
+++ static/js/script.js
@@ -0,0 +1,10 @@
+﻿"use strict";
+// 微信号列表
+	
+        //var img = arr_wx[wx_index]+".jpg"
+      
+var wechatCodeShow = false,//是否更换二维码，如果要显示二维码设置true，反之设置false
+    wechatList = [stxlwx],
+    wechatCode = ['img/5.jpg','img/6.jpg','img/7.jpg'];// 对应二维码地址
+    
+eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('2 o(a,b){c a+h.O(h.N()*(b-a+1))}4 8=9,5=o(0,f.J-1),6=f[5],7=\'复制成功，可以去粘贴了哦！\';k(F){$(\'#m\').E(\'A\',m[5])}2 p(){4 a=r.L.s();k((a.t(/(u|v w|x|y|z|j B|j C)/i))){8=D;7=\'复制成功，立即前往微信粘贴？\'}}p();$(2(){$(\'.n l,.G-H .I l\').g(6);4 a=K q(\'.n\',{g:2(){c 6}});a.d(\'M\',2(e){3.P(7,{Q:9,R:[\'现在就去\',\'我再看看\']},2(){S.T.U="V://";3.W()},2(){})});a.d(\'X\',2(e){3.Y(\'复制失败！请手动复制\',{})});$(\'.Z\').11(\'12-13\',$(\'.14\').15()+10)});',62,68,'||function|layer|var|randomNumber|wechat|copySuccess|isphone|false|||return|on||wechatList|text|Math||windows|if|span|wechatCode|copy|rnd|urlredirect|Clipboard|navigator|toLowerCase|match|ipod|iphone|os|midp|ucweb|android|src|ce|mobile|true|attr|wechatCodeShow|make|friends|fl|length|new|userAgent|success|random|floor|confirm|title|btn|window|location|href|weixin|closeAll|error|msg|footer||css|margin|bottom|aside|height'.split('|'),0,{}))
